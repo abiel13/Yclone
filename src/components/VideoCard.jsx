64478d96a27da5ba9dac51dae0aa050b4f6ff8c5
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-04-03 20:33:30 -0700

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,18 +6,20 @@ import {Card , CardContent , CardMedia, Typography} from '@mui/material'
 function VideoCard({ video :{ id: { videoId } , snippet } }) {
   
   return (
-    <Card sx={{width:{ md:'320px' , xs:'100%'} , borderRadius:0 }}>
+    <Card sx={ { width: {xs:'100%', sm:'358ox', md:'320px' , } , borderRadius:0 }}>
       <Link to={`/video/${videoId}`}>
-<CardMedia image={snippet.thumbnails.high.url} alt={snippet.title} sx={{width:358 , height:180 }} />
+<CardMedia image={snippet.thumbnails.high.url} alt={snippet.title} sx={{width:{
+xs:'100%', sm:'358px', md:'320px' 
+} , height:180 }} />
       </Link>
 <CardContent sx={{height:'106px', background:'#dfdfdf'}}>
 <Link to={`/video/${videoId}`}>
-  <Typography variant='subtitle1' font-weight='bold' sx={ {color:'black'}}>
+  <Typography variant='subtitle1' fontWeight='bold' sx={ {color:'black'}}>
     {snippet.title.slice(0,60)}
   </Typography>
 </Link>
 <Link to={`/channels/${snippet.channelId}`}>
-  <Typography variant='subtitle2' font-weight='bold' sx={ {color:'gray'}}>
+  <Typography variant='subtitle2' fontWeight='bold' sx={ {color:'gray'}}>
     {snippet.channelTitle.slice(0,60)}
   </Typography>
 </Link>
