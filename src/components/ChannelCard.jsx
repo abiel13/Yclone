0afed8d7628388ba8470c2721452e6aafa030297
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-04-04 11:09:26 -0700

    moving forward

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card, Box, CardContent, CardMedia, Typography } from "@mui/material";
 
-function ChannelCard() {
+function ChannelCard({ channel }) {
   return (
-    <div>ChannelCard</div>
-  )
+    <Box
+      sx={{
+        borderRadius: "20px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: { xs: "100%", sm: "356px", md: "320px" },
+        height:'326px',
+        margin:'auto'
+      }}
+    >
+      <Link to={`/channel/${channel.id.channelId}`}>
+        <CardContent
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            textAlign: "center",
+            color: "black",
+          }}
+        >
+          <CardMedia
+            image={channel.snippet.thumbnails.high.url}
+            alt={channel.snippet.title}
+            sx={{ borderRadius: "50%", width: "180px", height: "180px" }}
+          />
+          <Typography variant="h6">{channel.snippet.title}</Typography>
+        </CardContent>
+      </Link>
+    </Box>
+  );
 }
 
-export default ChannelCard
+export default ChannelCard;
--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { React, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Videos, ChannelCard } from "./";
+import { fetchApi } from "../utils/fetchapi";
 
 function Channels() {
-  return (
-    <div>Channels</div>
-  )
+  const { id } = useParams();
+  const [channelid, setchannelid] = useState([]);
+  const [Videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchApi(`channels?part=snippet&id=${id}`).then((data) =>
+      setchannelid(data.data.items[0])
+    );
+    fetchApi(`search?channelId=${id}&part=snippet&order=date`).then((data) =>
+      setVideos(data.data.items)
+    );
+  }, [id]);
+
+  return <Box sx={{minHeight:'95vh'}}>
+    <Box>
+      <div style={{color:'red' , height:'300px' , zIndex:'10'}} />
+      <ChannelCard channel={channelid} />
+    </Box>
+  </Box>;
 }
 
-export default Channels
+export default Channels;
