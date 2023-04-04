76e20579a09f4309af68575ecb2362669de22d35
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-04-03 20:13:35 -0700

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {Card , CardContent , CardMedia} from '@mui/material'
+import {Card , CardContent , CardMedia, Typography} from '@mui/material'
 
 
 function VideoCard({ video :{ id: { videoId } , snippet } }) {
-  console.log(videoId , snippet)
+  
   return (
-    <Card>
+    <Card sx={{width:{ md:'320px' , xs:'100%'} , borderRadius:0 }}>
       <Link to={`/video/${videoId}`}>
 <CardMedia image={snippet.thumbnails.high.url} alt={snippet.title} sx={{width:358 , height:180 }} />
       </Link>
+<CardContent sx={{height:'106px', background:'#dfdfdf'}}>
+<Link to={`/video/${videoId}`}>
+  <Typography variant='subtitle1' font-weight='bold' sx={ {color:'black'}}>
+    {snippet.title.splice(0,60)}
+  </Typography>
+</Link>
+<Link to={`/channels/${snippet.channelId}`}>
+  <Typography variant='subtitle2' font-weight='bold' sx={ {color:'gray'}}>
+    {snippet.channelTitle.splice(0,60)}
+  </Typography>
+</Link>
+</CardContent>
+
     </Card>
   )
 }
