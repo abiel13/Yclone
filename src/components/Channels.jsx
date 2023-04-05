6a917255a7142bc17b01407fa249c5bec1f99aab
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-04-05 02:36:41 -0700

    Update Channels.jsx

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,13 +1,13 @@
 import { React, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
-import { Videos, ChannelCard } from "./";
+import { Videos , ChannelCard } from "./";
 import { fetchApi } from "../utils/fetchapi";
 
 function Channels() {
   const { id } = useParams();
   const [channelid, setchannelid] = useState([]);
-  const [Videos, setVideos] = useState([]);
+  const [Vid, setVid] = useState([]);
 
   useEffect(() => {
     fetchApi(`channels?part=snippet&id=${id}`).then((data) => setchannelid(data.data.items[0])
@@ -16,10 +16,10 @@ function Channels() {
     );
   }, [id]);
 
- console.log(channelid)
+ console.log(channelid , Vid)
 
   return <Box sx={{minHeight:'95vh'}}>
-    <Videos video={Videos} />
+    <Videos video={Vid} />
   </Box>;
 }
 
