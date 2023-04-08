ba284b0259bd20a69b39442289c3aebbe03eb6b5
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-04-07 23:08:36 -0700

    Update Channels.jsx

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import { React, useReducer, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 import { Videos, ChannelCard } from "./";
