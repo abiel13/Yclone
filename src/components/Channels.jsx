60d3d569ef8c3e621f2f3d323230231822f3ef55
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-04-07 22:24:04 -0700

    Update Channels.jsx

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,27 +1,32 @@
 import { React, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
-import { Videos , ChannelCard } from "./";
+import { Videos, ChannelCard } from "./";
 import { fetchApi } from "../utils/fetchapi";
 
 function Channels() {
   const { id } = useParams();
-  const [channelid, setchannelid] = useState([]);
+  const [c, setc] = useState([]);
   const [Vid, setVid] = useState([]);
 
   useEffect(() => {
-    fetchApi(`channels?part=snippet&id=${id}`).then((data) => console.log(data)
+    fetchApi(`channels?part=snippet&id=${id}`).then((data) =>{
+      console.log(data.data.items)
+      setc(data.data.items)
+    console.log(c)
+    }
     );
-    fetchApi(`search?channelId=${id}&part=snippet&order=date`).then((data) => console.log(data)
+    fetchApi(`search?channelId=${id}&part=snippet&order=date`).then((data) =>
+      console.log(data)
     );
   }, [id]);
 
-
-
-  return <Box sx={{minHeight:'95vh'}}>
-    {/* <Videos video={Vid} /> */}
-    channels page
-  </Box>;
+  return (
+    <Box sx={{ minHeight: "95vh" }}>
+      {/* <Videos video={Vid} /> */}
+      channels page
+    </Box>
+  );
 }
 
 export default Channels;
